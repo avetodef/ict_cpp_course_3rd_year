09cda7d432a8521877d5c121a6fd07044a6a24ce
fedoteva <sofiafedoteva@gmail.com>
2023-09-16 13:09:54 +0300

    lab two done

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -1,63 +1,60 @@
 //
 // Created by Софья on 12.09.2023.
-// A super prime number is a number that is prime itself
-// and each time it is diveded by 10 all of those numbers are prime as well
 //
 #include <iostream>
 #include <vector>
 #include <map>
 using namespace std;
 
-#define SNILS_LENGTH 9
 
 class LabTwo{
 public:
 
     bool superPrime(int n){
-        if (n<10 && isPrime(n)) {return true;}
-        if (isPrime(n)) {return superPrime(n/10);}
+        if (n<10 && isPrime(n)) {return true;}// A super prime number is a number that is prime itself
+        if (isPrime(n)) {return superPrime(n/10);}// and each time it is diveded by 10 all of those numbers are prime as well
         return false;
     }
 
-    bool checkSNILS(int snils){
-        int snilsBackup = snils;
+    static bool checkSNILS(long long snils){
+
+        long long snilsBackup = snils;
         int lastDigits[2];
-        lastDigits[1] = snils/10; lastDigits[0] = snils/10;
-        cout << lastDigits << endl;
+
+        lastDigits[1] = snils%10;
+        lastDigits[0] = (snils/10)%10; //get last two digits in array
+
+        int* lastDigCheck = getFinalDigits(snilsBackup);
+
         return (checkSNILSSequence(snilsBackup)
-        && (lastDigits == getFinalDigits(snilsBackup)));
+        && (lastDigits[0] == lastDigCheck[0]) && (lastDigCheck[1] == lastDigits[1]) ); //check
     }
 
 
     vector<int> greedyCoins(int sum){
         int coinsSystem[4] = {1, 2, 5, 10};
-        vector<int> coinsVector;
-        vector<int> error = {-1};
-
-        for (int & i : coinsSystem){
-            while (sum >= i){
-                sum -= i;
-                coinsVector.push_back(i);
+        int n = 4; //length of array
+        vector <int> coins;
+        for (int i = n-1; i >=0; i--){ // from most valuable to less valuable coin
+            while (sum >= coinsSystem[i]){
+                sum -= coinsSystem[i];
+                coins.push_back(coinsSystem[i]);
             }
         }
-        if (sum == 0) return coinsVector;
-        return error;
+        return coins;
     }
 
-    void targetShooting(int shootings){
-        pair<int, int> center = getCenter();
-        cout << "Write down coordinates " << shootings << " times" << endl;
-        int x, y;
-        int pointsSum = 0;
-        for (int i = 0; i < shootings; i ++){
-            cin >> x, cin>>y;
-            pair<int, int> shot  = pair(int(x), int(y));
-            pointsSum += getPoints(center,shot);
-        }
-        string status = getStatus(pointsSum, shootings);
-        cout << "Your result is " << pointsSum << " points"<<endl;
-        cout << "Your status is " << status<< endl;
+    void playTargetGame(){
+        cout << "Hello there! Lets play target game." << endl<<
+        "Firstly, we need to understand which type of game would you like to play."<< endl <<
+        " Do you want to shot a certain number of times and try to get best result possible or "
+        "try to get a certain amount of points with least shots?" << endl << "If first, type 1. Else type 2" << endl;
+        int type; cin >> type;
+        cout << "Got you!" << endl << "Generating random center..." << endl;
+        if (type == 1) shootingWithKnownAmountOfShots();
+        else shootingWithTargetPoints();
     }
+
 
 
 private:
@@ -69,40 +66,37 @@ private:
         return true;
     }
     
-    static int * convertSNILSToArray(int s){
-        int snils[9];
-        s/100;//nah last two digits
-
+    static int * convertSNILSToArray(long long s){
+        int* snils = new int[9];
+        s = s/100;//nah last two digits
         for (int i = 9; i > 0; i--){
             snils[i] = s%10;
+            s = s / 10;
         }
         return snils;
     }
     
-    static bool checkSNILSSequence(int s){
-
+    static bool checkSNILSSequence(long long s){
        int* snils = convertSNILSToArray(s);
-
-        for (int i = 1 ; i < SNILS_LENGTH; i++){
+        for (int i = 1 ; i < 8; i++){
             if (snils[i-1] == snils[i] == snils[i+1]) return false;
         }
         return true;
     }
     
-    static int* getFinalDigits(int s){
+    static int* getFinalDigits(long long s){
         int* snils = convertSNILSToArray(s);
         int resultInt = 0;
-        int resArr[2] = {0,0};
-        for (int i = 0; i < SNILS_LENGTH; i++){
-            resultInt += (snils[i] * i);
+        int* resArr = new int [2]{0,0};
+        for (int i = 1; i < 10; i++){
+            resultInt += (snils[i] * (10 - i));
         }
-
         if (resultInt == 100 || resultInt == 101) return resArr;
         if (resultInt > 101){
-            int ost = resultInt / 101;
+            int ost = resultInt % 101;
             if (ost < 100) {
-                int ostArr[2];
-                ostArr[1] = ost/10; ostArr[0] = ost;
+                int* ostArr = new int[2];
+                ostArr[1] = ost%10; ostArr[0] = (ost/10)%10;
                 return ostArr;
             }
             if (ost == 100) return resArr;
@@ -112,14 +106,16 @@ private:
     }
 
     pair<int, int> getCenter(){
-        int x = rand() % 3 -3; //????????
-        int y = rand() % 3 -3;
+
+        int range = 3 - (-3) + 1;
+        int x = rand() % range + (-3);
+        int y = rand() % range + (-3);
         return pair(x, y);
     }
 
     int getPoints(pair<int,  int> center, pair<int, int> shot){
-        int xDiff = center.first - shot.first;
-        int yDiff = center.second - shot.second;
+        int xDiff = abs(center.first - shot.first);
+        int yDiff = abs(center.second - shot.second);
 
         if (xDiff == yDiff){
             if (xDiff <= 1) return 10;
@@ -135,11 +131,65 @@ private:
         return 0;
     }
 
-    string getStatus(int points, int shootings){
+    string getStatusForFirstTypeOfGame(int points, int shootings){
         int maxPoints = shootings * 10;
         if (points >= 0.9*maxPoints) return "COOL";
         if (points >= 0.75*maxPoints) return "NORM";
         if (points >= 0.6*maxPoints) return "COULD BE BETTER";
         return "NOOB";
+    }
+
+    string getStatusForSecondTypeOfGame(int points, int shots){
+        int bestResult = points / 10;
+        if (shots <= 0.9 * bestResult) return "COOL";
+        if (shots <= 0.75 * bestResult) return "NORM";
+        if (shots <= 0.6 * bestResult) return "COULD BE BETTER";
+        return "NOOB";
+    }
+
+    void printResult(int pointsSum, int shootings, pair<int, int> center){
+        string status = getStatusForFirstTypeOfGame(pointsSum, shootings);
+        cout << "Center was located at (" << center.first << "," << center.second << ")" << endl;
+        cout << "Your result is " << pointsSum << " points "<< "of " << shootings * 10 << endl;
+        cout << "Your status is " << status<< endl;
+    }
+
+    void shootingWithKnownAmountOfShots(){
+        cout << "Enter how many times you want to shot" << endl;
+        int shootings; cin >> shootings;
+        pair<int, int> center = getCenter();
+        cout << "Write down coordinates " << shootings << " times" << endl;
+        int x, y;
+        int pointsSum = 0;
+        for (int i = 0; i < shootings; i ++){
+            cin >> x, cin>>y;
+            pair<int, int> shot  = pair(int(x), int(y));
+            int points = getPoints(center, shot);
+            cout << "You got " << points << " points for this shot" << endl;
+            pointsSum += points;
+            cout << "Total points so far: " << pointsSum << endl;
+        }
+        string status = getStatusForFirstTypeOfGame(pointsSum, shootings);
+        printResult(pointsSum, shootings, center);
+    }
+
+    void shootingWithTargetPoints(){
+        pair<int, int> center = getCenter();
+        cout << "How much points do you want? " << endl;
+        int pointsDesired; cin >> pointsDesired;
+        int x, y;
+        int pointsSum = 0;
+        int shootingsCount = 0;
+        while (pointsSum < pointsDesired){
+            cin >> x, cin>>y;
+            pair<int, int> shot  = pair(int(x), int(y));
+            int points = getPoints(center, shot);
+            cout << "You got " << points << " points for this shot" << endl;
+            pointsSum += points;
+            cout << "Total points so far: " << pointsSum << endl;
+            shootingsCount+= 1;
+        }
+        cout << "You got " << pointsDesired << " in " << shootingsCount << " shots" << endl;
+        cout << "Your status is " << getStatusForSecondTypeOfGame(pointsDesired, shootingsCount) << endl;
     }
 };
--- main.cpp
+++ main.cpp
@@ -4,30 +4,28 @@
 #include "lab02/lab02.cpp"
 
 int main() {
-//    cout<< "Lab One" <<endl;
-//    LabOne labOne;
-//    double area = labOne.polygonArea();
-//    cout << "Area of polygon is " << area << endl;
-//
-//    cout << "Lab Two. Exercise one" <<endl;
-    LabTwo labTwo;
-//    int n;
-//    cout << "Enter number to check if it is super prime" << endl;
-//    cin >> n;
-//    bool isSuperPrime = labTwo.superPrime(n);
-//    if (isSuperPrime) cout << n << " is a super prime number" << endl;
-//    else cout << n << " is not a super prime number" << endl;
+    cout<< "Lab One" <<endl;
+    LabOne labOne;
+    double area = labOne.polygonArea();
+    cout << "Area of polygon is " << area << endl;
 
-    cout << "Exercise two" << endl;
-    cout << "Enter your SNILS" << endl;
-    int snils;
+
+    LabTwo labTwo;
+    int n;
+    cout << "Lab Two. Exercise one" <<endl<< "Enter number to check if it is super prime" << endl;
+    cin >> n;
+    bool isSuperPrime = labTwo.superPrime(n);
+    if (isSuperPrime) cout << n << " is a super prime number" << endl;
+    else cout << n << " is not a super prime number" << endl;
+
+    cout << "Exercise two" << endl<< "Enter your SNILS" << endl;
+    long long snils;
     cin >> snils;
-    bool snilsRes = labTwo.checkSNILS(snils);
+    bool snilsRes = LabTwo::checkSNILS(snils);
     if (snilsRes) cout << "Your SNILS is valid" << endl;
     else cout << "Your SNILS is invalid" << endl;
 
-    cout << "Exercise three" << endl;
-    cout << "Enter desired coin value" << endl;
+    cout << "Exercise three" << endl<< "Enter desired coin value" << endl;
     int coins;
     cin >> coins;
     vector<int> result = labTwo.greedyCoins(coins);
@@ -35,10 +33,8 @@ int main() {
     for (int i : result) cout << i << " ";
     cout << endl;
 
-    cout << "Exercise three" <<endl << "Generating random center..." << endl << "Now enter how many times you want to shot" << endl;
-    int shootings;
-    cin >> shootings;
-    labTwo.targetShooting(shootings);
+    cout << "Exercise three" <<endl ;
+    labTwo.playTargetGame();
 
     return 0;
 }
